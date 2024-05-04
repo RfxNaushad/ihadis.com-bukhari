e5a9fe2cbff04e2217d2595e2ede78f03f4a2bbd
RfxNaushad <rfxhridoy@gmail.com>
2024-05-05 02:05:33 +0600

    feat: sidebar hover

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faUser,
@@ -6,24 +6,45 @@ import {
   faBell,
   faEnvelope,
   faChartBar,
-} from '@fortawesome/free-solid-svg-icons'
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const iconData = [
+  { id: 1, icon: faHome, label: "Home" },
+  { id: 2, icon: faUser, label: "Profile" },
+  { id: 3, icon: faCog, label: "Settings" },
+  { id: 4, icon: faBell, label: "Notifications" },
+  { id: 5, icon: faEnvelope, label: "Messages" },
+  { id: 6, icon: faChartBar, label: "Reports" },
+];
 
 const Sidebar = () => {
+  const [hoveredIcon, setIconHover] = useState(null);
+
   return (
     <div
       className="bg-gray-800 text-white p-4 
       flex fixed bottom-0 left-0 w-full justify-around 
       xl:flex-col xl:justify-center xl:items-center xl:w-24 xl:fixed xl:left-0 xl:top-24 
-      xl:h-[calc(100vh-4rem)] xl:space-y-14"
+      xl:h-[calc(100vh-4rem)] xl:space-y-6"
     >
-      <FontAwesomeIcon icon={faHome} className='size-7' />
-      <FontAwesomeIcon icon={faUser} className='size-7' />
-      <FontAwesomeIcon icon={faCog} className='size-7' />
-      <FontAwesomeIcon icon={faBell} className='size-7' />
-      <FontAwesomeIcon icon={faEnvelope} className='size-7' />
-      <FontAwesomeIcon icon={faChartBar} className='size-7' />
+            {iconData.map((item) => (
+        <div
+          key={item.id}
+          className="p-4 hover:bg-gray-200 rounded transition-colors" onMouseEnter={() => setIconHover(item.id)}
+          onMouseLeave={() => setIconHover(false)} 
+        >
+          <FontAwesomeIcon
+            icon={item.icon}
+            className={`size-7 transition-colors ${
+              hoveredIcon === item.id ? 'text-green-500' : 'text-white' // Change color based on hover state
+            }`}
+          />
+        </div>
+      ))}
     </div>
-  )
-}
 
-export default Sidebar
+  );
+};
+
+export default Sidebar;
