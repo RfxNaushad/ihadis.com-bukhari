ec96800ad2f2c02357e15dd8d42e4839ae52235c
RfxNaushad <rfxhridoy@gmail.com>
2024-05-05 01:37:00 +0600

    feat: hover effect left section hexagon

--- pages/components/LeftSection.js
+++ pages/components/LeftSection.js
@@ -190,7 +190,11 @@ const LeftSection = ({ isSidebarOpen }) => {
                 onMouseLeave={() => setHoveredItem(null)}
               >
                 <div
-                  className="w-12 h-12 relative flex items-center justify-center bg-gray-100 text-gray"
+                  className={`w-12 h-12 relative flex items-center justify-center bg-gray-100 text-gray ${hoveredItem === index ? 'bg-green-500' : 'bg-gray-100'}`}
+
+
+
+
                   style={{
                     clipPath:
                       "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
